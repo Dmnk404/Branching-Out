b984d0dd12d2a7144063e7dd0cf0cf85981e3745
Dominik Melzner <dominik@Mac.fritz.box>
2025-06-05 09:42:10 +0200

    Added function and logic to filter users by age.

--- filter_users.py
+++ filter_users.py
@@ -10,12 +10,26 @@ def filter_users_by_name(name):
     for user in filtered_users:
         print(user)
 
+def filter_users_by_age(age):
+    with open("users.json", "r") as file:
+        users = json.load(file)
+
+    filtered_users_age = [user for user in users if user["age"] == age]
+
+    if len(filtered_users_age) > 0:
+        for user in filtered_users_age:
+         print(user)
+    else:
+        print("No users found")
 
 if __name__ == "__main__":
-    filter_option = input("What would you like to filter by? (Currently, only 'name' is supported): ").strip().lower()
+    filter_option = input("What would you like to filter by? (Currently, only 'name' and 'age' are supported): ").strip().lower()
 
     if filter_option == "name":
         name_to_search = input("Enter a name to filter users: ").strip()
         filter_users_by_name(name_to_search)
+    elif filter_option == "age":
+        age_to_search = input("Enter an age to filter users: ").strip()
+        filter_users_by_age(int(age_to_search))
     else:
         print("Filtering by that option is not yet supported.")
